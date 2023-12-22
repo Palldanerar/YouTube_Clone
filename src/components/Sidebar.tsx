f970ac103e2c9ed08349270878e66a0eb749e731
Palldanerar <alexkharin86@gmail.com>
2023-12-22 09:09:04 +0300

    Update component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,18 @@
 import { Stack } from "@mui/material";
-
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import HomeIcon from '@mui/icons-material/Home';
 import CodeIcon from '@mui/icons-material/Code';
-import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
 import SchoolIcon from '@mui/icons-material/School';
 import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 
 const categories = [
   { name: 'New', icon: <HomeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
-  { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
-  { name: 'Movie', icon: <OndemandVideoIcon />, },
   { name: 'Gaming', icon: <SportsEsportsIcon />, },
-  { name: 'Live', icon: <LiveTvIcon />, },
-  { name: 'Sport', icon: <FitnessCenterIcon />, },
-  { name: 'Gym', icon: <FitnessCenterIcon />, },
 ];
 
 
--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -3,7 +3,7 @@ import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
-    <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 3 }}>
         <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
             <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title}
                 sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
